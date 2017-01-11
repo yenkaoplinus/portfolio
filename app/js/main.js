c4043c8aad6138c2cf1e69ebf03e0fdfa8b312d7
Shaun Janssens <shaunjanssens@gmail.com>
2017-01-11 22:45:51 +0100

    Menu overlay problem

--- app/js/main.js
+++ app/js/main.js
@@ -1,11 +1,9 @@
 $(function () {
-    console.log("HALLO YENKA ❤️");
+    console.log("ELAAA, KOM JE SNUISTEREN IN MIJN CODE? ❤️");
 
     // Menu op top
     var viewportHeight = $(window).height();
     var menuHeight = $('.mainnav').outerHeight();
-
-    console.log(menuHeight);
 
     $(window).bind('scroll', function () {
         if ($(window).scrollTop() > viewportHeight) {
@@ -15,5 +13,15 @@ $(function () {
             $('.mainnav').removeClass('fixed');
             $('.mainnav').next().css('margin-top', '0px');
         }
+    });
+
+    $(".mainnav a").on('click', function(event) {
+      var selectedElement = $(this).data("element");
+
+      if(selectedElement == "contact") {
+        $("." + selectedElement).css("padding-top", menuHeight + 50);
+      } else {
+        $("." + selectedElement).css("padding-top", menuHeight);
+      }
     });
 });
